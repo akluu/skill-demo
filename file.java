9c942848694aea0ddb422326e3536e3ad67d1e29
akluu <81354105+akluu@users.noreply.github.com>
2022-05-01 18:31:28 -0700

    added a method and a test that should fail

---
+++ file.java
@@ -0,0 +1,5 @@
+public class file{
+    public static boolean isEven(int num){
+        return num % 2 == 1;
+    }
+}
---
+++ fileTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class fileTest {
+    @Test
+    public void isEvenTest(){
+        assertEquals(true, file.isEven(2));
+    }
+}
